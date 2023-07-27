45d213b4be06e3e6568c4c44b635b1dcfc316d6e
Prabjot Singh <prabjot.singh@andersencorp.com>
2023-07-27 09:01:31 -0400

    Implemented the home screen

--- ToDoAppEndToEndTests/ContentView.swift
+++ ToDoAppEndToEndTests/ContentView.swift
@@ -9,80 +9,127 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    // MARK: - Properties
+    @State private var title: String = ""
+    @State private var priority: String = "Medium"
+    @State private var taskBody: String = ""
+    @State private var message: String = ""
+    
     @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @FetchRequest(fetchRequest: Task.allTasksFetchRequest()) private var allTasks: FetchedResults<Task>
+    
+    @State private var selectedTask: Task?
+    
+    // MARK: - Functions
+    private func saveTask() {
+        if title.isEmpty {
+            return
+        }
+        
+        do {
+            // see if the task is already added
+            if let _ = Task.by(title: title) {
+                message = "Task is already added"
+            } else {
+                
+                let task = Task(context: viewContext)
+                task.title = title
+                task.priority = priority
+                task.body = taskBody
+                
+                // save
+                try viewContext.save()
+            }
+        } catch {
+            print(error)
+        }
+        
+        title = ""
+        priority = "Medium"
+        taskBody = ""
+    }
+    
+    // MARK: - Body
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+            VStack(spacing: 20) {
+                TextField("Enter title", text: $title)
+                    .textFieldStyle(.plain)
+                    .accessibilityIdentifier("titleTextField")
+                
+                TextField("Enter description", text: $taskBody)
+                    .textFieldStyle(.plain)
+                    .accessibilityIdentifier("taskBodyTextField")
+                
+                Picker("Priority", selection: $priority) {
+                    Text("Low").tag("Low")
+                    Text("Medium").tag("Medium")
+                    Text("High").tag("High")
+                }.pickerStyle(.segmented)
+                
+                Button("Save") {
+                    saveTask()
+                } //: Button
+                .padding(10)
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity)
+                .background(Color.gray)
+                .clipShape(RoundedRectangle(cornerRadius: 10.0, style: .continuous))
+                .accessibilityIdentifier("saveTaskButton")
+                
+                Text(message)
+                    .accessibilityIdentifier("messageText")
+                
+                List {
+                    ForEach(allTasks) { task in
+                        HStack {
+                            Text(task.title ?? "")
+                            Spacer()
+                            
+                            if task.isFavorite {
+                                Image(systemName: "heart.fill")
+                                    .foregroundColor(.red)
+                            }
+                            
+                            Text(task.priority ?? "")
+                                .frame(maxWidth: 100)
+                        } //: HStack
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            self.selectedTask = task
+                        }
+                        .sheet(item: $selectedTask) {
+                            
+                        } content: { task in
+                            //TaskDetailView(task: task)
+                        }
+                    } //: ForEach
+                    .onDelete { indexSet in
+                        indexSet.forEach { index in
+                            let task = allTasks[index]
+                            viewContext.delete(task)
+                            do {
+                                try viewContext.save()
+                            } catch {
+                                print(error)
+                            }
+                        }
                     }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+                } //: List
+                .accessibilityIdentifier("taskList")
+                
+                Spacer()
+            } //: VStack
+            .padding()
+            .navigationTitle("Tasks")
+        } //: NavigationView
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
+// MARK: - Preview
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        let persistedContainer = CoreDataManager.shared.persistentContainer
+        ContentView().environment(\.managedObjectContext, persistedContainer.viewContext)
     }
 }
