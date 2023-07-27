c9523a9e74834b9d37d06197a99cf0d8d93d9ba7
Prabjot Singh <prabjot.singh@andersencorp.com>
2023-07-27 07:31:53 -0400

    Initial app setup

---
+++ ToDoAppEndToEndTests/ToDoAppEndToEndTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppEndToEndTestsApp.swift
+//  ToDoAppEndToEndTests
+//
+//  Created by M_2195552 on 2023-07-27.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppEndToEndTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
